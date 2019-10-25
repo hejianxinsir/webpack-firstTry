0c7e293609f17a697ee99c9f94cdd12ba2ec123f
hejianxin <hejianxinsir@gmail.com>
2019-10-25 15:12:32 +0800

    引入 jquery

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './icon.png';
-import module1 from './module1'
-import module2 from './module2'
-module1()
-module2()
+import j from 'jquery'
 
 function component() {
-  const element = document.createElement('div');
+  var element = j('<div></div>');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello')
-  element.classList.add('janson')
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-
-  return element;
+  element.html(_.join(['hello', 'webpack'], ' '));
+  return element.get(0);
 }
 
 document.body.appendChild(component());
